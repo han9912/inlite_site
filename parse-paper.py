291155fac86d7d18090de40a522e5364db18388c
hanx <han9912@outlook.com>
2022-08-25 18:36:41 +0800

    revise sql script

--- parse-paper.py
+++ parse-paper.py
@@ -24,31 +24,40 @@ if __name__ == "__main__":
     print('before with open method')
     with open(filename, encoding='UTF-8') as file:
         print('open with success')
-        data_record = null_record
+        data_record = {
+        'title': NULL,
+        'author': NULL,
+        'time': NULL,
+        'content': NULL,
+        }
         for line in tqdm.tqdm(file):
             # add_employee = ("INSERT INTO record "
             #    "(title, author, time, content) "
             #    "VALUES (%s, %s, %s, %s)")
             # data_employee = ('Geert', 'Vanderkelen', tomorrow, 'M', date(1977, 6, 14))
-            if line == '<REC>' and data_record != null_record:
+            test_condition = '<REC>\n'
+            if line == '<REC>\n' and data_record != null_record:
                 cursor.execute(add_record, data_record)
-                data_record = null_record
+                data_record = {
+                'title': NULL,
+                'author': NULL,
+                'time': NULL,
+                'content': NULL,
+                }
+                print('the record is record in row', cursor.lastrowid)
             
             if line.startswith('<title>='):
                 data_record['title'] = line.removeprefix('<title>=') 
             if line.startswith('<authors>='):
                 data_record['author'] = line.removeprefix('<authors>=') 
-            if line.startswith('<pub_data>='):
-                data_record['time'] = line.removeprefix('<pub_data>=') 
+            if line.startswith('<pub_time>='):
+                data_record['time'] = line[11:21].replace('/', '-') 
             if line.startswith('<content>='):
-                data_record['content'] = line.removeprefix('<content>=')[11:21].replace('/', '-') 
+                data_record['content'] = line.removeprefix('<content>=') 
 
-            # emp_no = cursor.lastrowid
-  
+            # emp_no = cursor.lastrowid         
 
-            # Make sure data is committed to the database
-else:
-    print('fail to name equals main')
+# Make sure data is committed to the database
 cnx.commit()
 
 cursor.close()
